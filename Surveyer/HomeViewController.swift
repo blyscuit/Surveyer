f872ae5dc4311670f6b6ce5cb03850a4937c6fcf
Bliss <pisit.w@23perspective.com>
2020-06-10 21:41:02 +0700

    [ edit ] home view with CollectionView and pageControl

--- Surveyer/HomeViewController.swift
+++ Surveyer/HomeViewController.swift
@@ -8,12 +8,34 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: BaseViewController {
+    
+    var surveyCollectionView: UICollectionView?
+    
+    lazy var pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.translatesAutoresizingMaskIntoConstraints = false
+        return pc
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = NSLocalizedString("Surveys", comment: "")
+    }
+    
+    override func insertUIViews() {
+        let surveyCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(surveyCollectionView)
+        self.surveyCollectionView = surveyCollectionView
+        
+        view.addSubview(pageControl)
+        pageControl.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        pageControl.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        pageControl.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20).isActive = true
+        pageControl.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI_2));
+        pageControl.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        pageControl.numberOfPages = 10
     }
 
 }
---
+++ Surveyer/ShareViewController/BaseViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BaseViewController.swift
+//  Surveyer
+//
+//  Created by Pisit W on 10/6/2563 BE.
+//  Copyright © 2563 blyscuit. All rights reserved.
+//
+
+import UIKit
+
+//Any share variable or method
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        insertUIViews()
+    }
+    
+    func insertUIViews() {}
+
+}
